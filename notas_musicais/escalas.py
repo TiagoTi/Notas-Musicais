5b665726bfa41cbea252f0bd0efa3ce240c705e2
tiago.cipriano <tiago.cipriano@luizalabs.com>
2023-11-06 22:06:42 -0300

    feat: adiciona escalas

---
+++ notas_musicais/escalas.py
@@ -0,0 +1,41 @@
+NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
+GRAUS = 'I II III IV V VI VII'.split()
+ESCALAS = {
+    'maior': {'intervalo': (0, 2, 4, 5, 7, 9, 11), 'grau': GRAUS},
+    'minor': {'intervalo': (0, 2, 3, 5, 7, 8, 10), 'grau': GRAUS},
+    'pentatonica': {
+        'intervalo': (0, 2, 4, 7, 9),
+        'grau': 'I II III V VI'.split(),
+    },
+    'pentatonica menor': {
+        'intervalo': (0, 3, 5, 7, 10),
+        'grau': 'I III IV V VII'.split(),
+    },
+}
+
+DOZE_NOTAS = len(NOTAS)
+
+
+def escalas(tonica, tonalidade):
+    """
+    escalas('C', 'maior')
+    {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+
+    escalas('A', 'maior')
+    {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+
+    >>> escalas('D', 'pentatonica')
+    {'notas': ['D', 'E', 'F#', 'A', 'B'], 'graus': ['I', 'II', 'III', 'V', 'VI']}
+
+    >>> escalas('C', 'pentatonica menor')
+    {'notas': ['C', 'D#', 'F', 'G', 'A#'], 'graus': ['I', 'III', 'IV', 'V', 'VII']}
+    """
+    intervalo_escala = ESCALAS[tonalidade]['intervalo']
+    notas = list()
+    tonica_posicao = NOTAS.index(tonica)
+
+    for intervalo in intervalo_escala:
+        posicao_nota = (intervalo + tonica_posicao) % DOZE_NOTAS
+        notas.append(NOTAS[posicao_nota])
+
+    return {'notas': notas, 'graus': ESCALAS[tonalidade]['grau']}
